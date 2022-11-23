8dd0f65fe648f597e3e01e05337a7a14f1c4ed28
Vitali Kartsivadze <vi.kartsivadze@gmail.com>
2022-11-23 16:04:31 +0400

    added footer and dashboard scratch

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                    <a href="{{ route('main') }}">
+                        <x-jet-application-mark class="block h-20 w-auto" />
                     </a>
                 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth');
+})->middleware('auth')->name('main');
 
 Route::middleware([
     'auth:sanctum',
