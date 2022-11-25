96ca5b635d5463b6a42a2117e0316a52a723571e
Vitali Kartsivadze <vi.kartsivadze@gmail.com>
2022-11-25 19:35:24 +0400

    added actionable trait

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Nova\Actions\Actionable;
 
 class Plan extends Model
 {
-    use HasFactory;
+    use HasFactory, Actionable;
 }
